6a1278f46be8fb5d0fe33a1392d807836a3e873a
Gabriel C <gabrielcorralcardenas@gmail.com>
2022-08-18 12:38:19 -0700

    Code snippet example

---
+++ inputAdd/WarmUp-8-18-2022/Snippet.java
@@ -0,0 +1,13 @@
+public class Snippet {
+    public static void main(String args[]) {
+        double solution = 1/2;
+        System.out.println(solution);
+    }
+}
+// data types are important in java because only the specified data types can be stored in memory else it will go to the default of the data type.
+public class Snippet2 {
+    public static void main(String args[]) {
+        douvle solution = 1.0/2.0;
+        System.out.println(solution);
+    }
+}
