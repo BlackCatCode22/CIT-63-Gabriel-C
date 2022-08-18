d0e9269fc6c5fc4c28afeb92b4430f2441fcdd03
Gabriel C <gabrielcorralcardenas@gmail.com>
2022-08-18 11:08:50 -0700

    Cleaner code rules

--- inputAdd/WarmUp-8-18-2022/Add.java
+++ inputAdd/WarmUp-8-18-2022/Add.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class Add {
     public static void main(String args[]) {
+        // Creating a Scanner object (numbers) for input
         Scanner numbers = new Scanner(System.in);
-        System.out.println("\nEnter two numbers: " + "-And press enter-");
-        int n1, n2, n3;
+        // Declaring variables.
+        int n1 = 0;
+        int n2 = 0;
+        int n3 = 0;
+        // Ask for input
+        System.out.println("\nEnter First number: " + "-And press enter-");
         n1 = numbers.nextInt();
+        System.out.println("\nEnter Second number: " + "-And press enter-");
         n2 = numbers.nextInt();
+        // Adds stored inputs and displays the total
         n3 = n1 + n2;
-        System.out.println("\nThe total is: " + n3);
+        System.out.println("\nThe total is: " + n3 + "\n\n");
     }
 }
--- inputAdd/WarmUp-8-18-2022/IfElse.java
+++ inputAdd/WarmUp-8-18-2022/IfElse.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class IfElse {
     public static void main(String args[]) {
+        // Creating a Scanner object (guesses) for input
         Scanner guesses = new Scanner(System.in);
+        // Declare variable
+        int n1 = 0;
+        // Ask for input
         System.out.println("\nGuess a number between 1-10  " + "Then Press Enter-");
-        int n1;
         n1 = guesses.nextInt();
+        // logic based on input to see if the number is equal to 6
         if(n1 != 6) {
             System.out.println("\nYou guessed wrong!\n\n");
         } else {
