dd2921afdab284872480fa2d663956b1b02db29e
Gabriel C <gabrielcorralcardenas@gmail.com>
2022-08-30 10:52:12 -0700

    warmup aug 30

--- WarmUpAug30/WarmUpAug30.java
+++ WarmUpAug30/WarmUpAug30.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class WarmUpAug30 {
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class WarmUpAug30 {
+   public static void hello() {
+        System.out.println("\n Hello from hello()!");
+    }
+    public static int addTwoNumbers(int num1, int num2) {
+        int result = 0;
+        result = num1 + num2;
+        return result;
+    }
+    public static void main(String[] args) {
+       System.out.println("\nWarmup Aug 30th 2022!");
+       hello();
+       int result = addTwoNumbers(7, 3);
+       System.out.println("\nThe sum of two numbers is " + result + "\n");
+    }
 }
