d10eecc294d906a0d742b451f8f20de99ba626f3
Gabriel C <gabrielcorralcardenas@gmail.com>
2022-09-06 13:13:47 -0700

    WarmUpSep06

---
+++ WarmUpSep06/WarmUpSep06.java
@@ -0,0 +1,11 @@
+package com.dennis.september;
+
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class WarmUpSep06 {
+    public static void main(String[] args) {
+        System.out.println("\nToday is September 6th!\n");
+    }
+}
+
